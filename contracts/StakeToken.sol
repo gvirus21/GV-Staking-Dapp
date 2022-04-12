fa5ae3427e02d26a7f816565852f23e4839d83b7
gourav <gvirus21yt@gmail.com>
2022-04-12 12:00:15 +0530

    hotfix: corrected contract name, - from GVToken to StakeToken

--- contracts/StakeToken.sol
+++ contracts/StakeToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GVToken is ERC20 {
+contract StakeToken is ERC20 {
     constructor() ERC20("StakeToken", "STK") {
         _mint(msg.sender, 1000000000000000000000000);
     }
