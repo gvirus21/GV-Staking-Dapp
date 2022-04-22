78c25e24aba57cce126fbd642dfd54044768e388
gourav <gvirus21yt@gmail.com>
2022-04-22 12:48:54 +0530

    updated gvtoken supply

--- contracts/GVToken.sol
+++ contracts/GVToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GVToken is ERC20 {
     constructor() ERC20("GVToken", "GVT") {
-        _mint(msg.sender, 1000000000000000000000000);
+        _mint(msg.sender, 1000000000000000000000000000);
     }
 }
 
