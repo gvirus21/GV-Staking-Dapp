4fe37b3d56d0d3f88fb5d68ac6519fbb958753e7
gourav <gvirus21yt@gmail.com>
2022-04-23 10:58:43 +0530

    corrected previous tests and updated them

--- test/tokenFarm_test.js
+++ test/tokenFarm_test.js
@@ -2,47 +2,36 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Token Farm Tests", async () => {
-
   let gvTokenAddress;
-  let stakeTokenAddress;
   let tokenFarmAddress;
 
+  let gvToken
   let tokenFarm;
+  let address1;
 
   before(async () => {
     let GVToken = await ethers.getContractFactory("GVToken");
-    let gvToken = await GVToken.deploy();
+    gvToken = await GVToken.deploy();
     await gvToken.deployed();
 
     gvTokenAddress = gvToken.address;
     console.log("GVToken is deployed to: ", gvTokenAddress);
 
-    let StakeToken = await ethers.getContractFactory("StakeToken");
-    let stakeToken = await StakeToken.deploy();
-    await stakeToken.deployed();
-
-    stakeTokenAddress = stakeToken.address;
-    console.log("StakeToken is deployed to: ", stakeTokenAddress);
-
     let TokenFarm = await ethers.getContractFactory("TokenFarm");
-    tokenFarm = await TokenFarm.deploy(gvTokenAddress, stakeTokenAddress);
+    tokenFarm = await TokenFarm.deploy(gvTokenAddress);
     await tokenFarm.deployed();
 
     tokenFarmAddress = tokenFarm.address;
     console.log("TokenFarm is deployed to: ", tokenFarmAddress);
   });
-    
-    beforeEach(async () => {
-        const [signer] = await ethers.getSigners()
-    })
 
-    it("TokenFarm Should deploy both GVToken and StakeToken addresses", async () => {
-      expect(await tokenFarm.gvToken()).to.equal(gvTokenAddress)
-      expect(await tokenFarm.stakeToken()).to.equal(stakeTokenAddress)
-    });
-    
-    it("Staking 0 eth should revert", async () => {
-        const amount = await ethers.utils.parseEther("0")
-        await expect(tokenFarm.stakeTokens(amount)).to.be.revertedWith('Staking amount must more than 0');
-    })
+  beforeEach(async () => {
+    [address1] = await ethers.getSigners();
+    const totalGVTokens = await gvToken.totalSupply();
+    await gvToken.transfer(address1.address, totalGVTokens)
+  });
+
+  it("TokenFarm Should deploy GVToken", async () => {
+    expect(await tokenFarm.gvToken()).to.equal(gvTokenAddress);
+  });
 });
