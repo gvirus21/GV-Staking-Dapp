3d2400d3ed7bf6ae51018ca047a454fe402c456f
gourav <gvirus21yt@gmail.com>
2022-04-30 20:19:20 +0530

    extracted inline css to style object in main.js

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,20 +1,28 @@
 import React, { useStaete, useEffect } from "react";
 
+const style = {
+  wrapper: "bg-gray-700 h-screen py-64 flex justify-center items-center -mt-24",
+  innerContainer: "h-40 flex flex-col justify-between ",
+  inputContainer: "flex",
+    input: "px-3 py-1 text-xl mx-5 rounded-xl w-32",
+  button: "px-3 py-1 bg-gray-300 rounded-md",
+};
+
 const Main = () => {
   return (
-    <div className="bg-gray-700 h-screen py-64 flex justify-center items-center -mt-24 ">
-      <div className="h-40 flex flex-col justify-between ">
-        <div className="flex">
+    <div className={style.wrapper}>
+      <div className={style.innerContainer}>
+        <div className={style.inputContainer}>
           <input
             type="Number"
             min="0"
             max="100"
-            className="px-3 py-1 text-xl mx-5 rounded-xl w-32"
+            className={style.input}
           />
-          <button className="px-3 py-1 bg-gray-300 rounded-md">Stake</button>
+          <button className={style.button}>Stake</button>
         </div>
-        <button className="px-3 py-1 bg-gray-300 rounded-md">Unstake</button>
-        <button className="px-3 py-1 bg-gray-300 rounded-md">
+        <button className={style.button}>Unstake</button>
+        <button className={style.button}>
           Issue rewards
         </button>
       </div>
